104c358ad91fe2c7dbb9ddf378618623e6689920
Aldi <podajaldi@gmail.com>
2024-06-18 14:04:36 +0200

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+void do_echo(char* input);
+
 int main() {
 
   char input[100];
@@ -13,17 +15,21 @@ int main() {
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = 0;
 
-    if (strncmp(input, "exit", 4) == 0)
-    {
+    if (strncmp(input, "exit", 4) == 0) {
       exit(EXIT_SUCCESS);
+    } else if (strncmp(input, "echo", 4) == 0) {
+      do_echo(input);
+      fflush(stdout);
     } else {
       printf("%s: command not found\n", input);
       fflush(stdout);
     }
-    
-
-    
   }
     
   return 0;
+}
+
+void do_echo(char* input) {
+  char* temp = strchr(input, ' ');
+  printf("%s\n", temp + 1);
 }
