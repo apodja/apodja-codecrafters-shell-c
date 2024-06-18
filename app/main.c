5e8cda45e0070897b5597b774589065e996b02e3
Aldi <podajaldi@gmail.com>
2024-06-18 17:00:19 +0200

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void do_echo(char* input);
+bool isBuiltIn(char* cmd);
+char* get_nth_arg(char* input, int n);
 
 int main() {
 
@@ -19,11 +22,20 @@ int main() {
       exit(EXIT_SUCCESS);
     } else if (strncmp(input, "echo", 4) == 0) {
       do_echo(input);
-      fflush(stdout);
-    } else {
+      
+    } else if (strncmp(input, "type", 4) == 0) {
+      char* cmdType = get_nth_arg(input, 2);
+      if (isBuiltIn(cmdType))
+      {
+        printf("%s is a shell builtin\n", cmdType);
+      } else {
+        printf("%s: not found\n", cmdType);
+      }
+      
+    }  else {
       printf("%s: command not found\n", input);
-      fflush(stdout);
     }
+    fflush(stdout);
   }
     
   return 0;
@@ -33,3 +45,50 @@ void do_echo(char* input) {
   char* temp = strchr(input, ' ');
   printf("%s\n", temp + 1);
 }
+
+bool isBuiltIn(char* cmd) {
+  char* builtInCmds[] = {"echo", "exit", "type"};
+
+  for(int i = 0; i < 3; i++) {
+    if (strcmp(builtInCmds[i], cmd) == 0)
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+char* get_nth_arg(char* input, int n) {
+  const char* delim = " ";
+  char* input_cpy = strdup(input);
+  int i = 1;
+  char* ret;
+
+  char* token = strtok(input_cpy, delim);
+  if (token != NULL)
+  {
+    if (n == 1)
+    {
+
+      ret = strdup(token);
+      free(input_cpy);
+      return ret;
+    } else {
+      while (token != NULL)
+      {
+        if (i == n)
+        {
+          ret = strdup(token);
+          free(input_cpy);
+          return ret;
+        }
+        token = strtok(NULL, delim);
+        i++;
+      }
+    }
+  }
+   
+  return NULL;
+}
+
